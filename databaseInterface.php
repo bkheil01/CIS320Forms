fa60ec3e882b8c852dbb3f17d348ad9a69c6bf18
bagree07 <bagree07@louisville.edu>
2015-04-12 20:58:19 -0400

    I changed all the files to php so that I could include the header file. Plus, I created add, modify, and delete forms.

---
+++ databaseInterface.php
@@ -0,0 +1,73 @@
+<?php include('header.php'); ?>
+
+<div class="navbar navbar-inverse navbar-static-top" style="margin: 55px;">
+	<div class="container">
+		<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+		</button>
+		<div class="collapse navbar-collapse navHeaderCollapse">
+			<div class="dropdown nav navbar-nav navbar-right" style="margin: 7px 0px 0px 7px;">
+		        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Add
+		        <span class="caret"></span></button>
+		        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="addStudent.php">Student</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="addTeacher.php">Teacher</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="addVolunteer.php">Volunteer</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="addDonor.php">Donor</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="addEvent.php">Event</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="addCourse.php">Course</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="addEmployee.php">Employee</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="addGrant.php">Grant</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="addDonation.php">Donation</a></li>
+		        </ul>
+      		</div>
+      		<div class="dropdown nav navbar-nav navbar-right" style="margin: 7px 0px 0px 7px;">
+		        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Modify
+		        <span class="caret"></span></button>
+		        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateStudent.php">Student</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateTeacher.php">Teacher</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateVolunteer.php">Volunteer</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateDonor.php">Donor</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateEvent.php">Event</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateCourse.php">Course</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateEmployee.php">Employee</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateGrant.php">Grant</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateDonation">Donation</a></li>
+		        </ul>
+      		</div>
+      		<div class="dropdown nav navbar-nav navbar-right" style="margin: 7px 0px 0px 7px;">
+		        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Delete
+		        <span class="caret"></span></button>
+		        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateStudent.php">Student</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateTeacher.php">Teacher</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateVolunteer.php">Volunteer</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateDonor.php">Donor</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="UpdateEvent.php">Event</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateCourse.php">Course</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateEmployee.php">Employee</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateGrant.php">Grant</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="updateDonation.php">Donation</a></li>
+		        </ul>
+      		</div>
+      		<div class="dropdown nav navbar-nav navbar-right" style="margin: 7px 0px 0px 7px;">
+		        <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Report
+		        <span class="caret"></span></button>
+		        <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Student</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Teacher</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Volunteer</a></li>
+		          	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Donor</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Event</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Course</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Employee</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Grant</a></li>
+		        	<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Donation</a></li>
+		        </ul>
+      		</div>
+		</div>
+	</div>
+</div>
